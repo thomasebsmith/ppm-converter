da5016ffd82e97f5dda2055f8eb12a6959717bcb
thsm <thsm@umich.edu>
2019-02-06 23:32:37 -0500

    Add inline documentation to convert.js

--- convert.js
+++ convert.js
@@ -2,6 +2,9 @@ var newMaxVal = 255;
 
 var errorEl = document.getElementById("error");
 var errorContentEl = document.getElementById("error-content");
+
+// error(msg) - Displays `msg` in a red-colored overlay at the top of the page
+//  and throws `msg`.
 var error = function(msg) {
   errorEl.style.display = "block";
   errorEl.style.backgroundColor = "red";
@@ -9,28 +12,37 @@ var error = function(msg) {
   throw msg;
 };
 
+// success(msg) - Displays `msg` in a green-colored overlay at the top of the
+//  page and throws `msg`.
 var success = function(msg) {
   errorEl.style.display = "block";
   errorEl.style.backgroundColor = "#00DD44";
   errorContentEl.textContent = msg;
 };
 
+// hideError() - Hides the success/error overlay at the top of the page.
 var hideError = function() {
   errorEl.style.display = "none";
 };
 
+// assert(condition, msg) - Calls error(msg) iff condition is not truthy.
 var assert = function(condition, msg) {
   if (!condition) {
     error(msg);
   }
 };
 
+// toString(charCodeArray) - Takes an array of numbers and converts the array
+//  to a string with each character in the string corresponding to the
+//  character with the char code in the corresponding part of the array.
 var toString = function(charCodeArray) {
   return charCodeArray.map(function(code) {
     return String.fromCharCode(code);
   }).join("");
 };
 
+// toBinary(str) - Performs the reverse of toString - Converts the string to
+//  an array of its char codes.
 var toBinary = function(str) {
   return str.toString().split("").map(function(c) {
     return c.charCodeAt(0);
@@ -39,37 +51,41 @@ var toBinary = function(str) {
 var binarySpace = toBinary(" ")[0];
 var binaryNewLine = toBinary("\n")[0];
 
-var padRight = function(str, len) {
-  str += "";
-  if (str.length >= len) {
-    return str;
-  }
-  return str + Array(len - str.length + 1).join(" ");
-};
-
 var whitespace = Array(256).fill(false);
 whitespace[9] = true;
 whitespace[10] = true;
 whitespace[11] = true;
 whitespace[12] = true;
 whitespace[32] = true;
+// isWhitespace(charCode) - Returns true iff charCode corresponds to a standard
+//  ASCII whitespace value (i.e. one for which the C function isspace() would
+//  return true).
 var isWhitespace = function(charCode) {
   return whitespace[charCode];
 };
 
+// convertP6toP3(p6File, callback) - Reads the contents of p6File, producing
+//  an error if the file format is invalid or unexpected. Once the file is
+//  parsed, calls `callback` with the new P3-formatted file.
 var convertP6toP3 = function(p6File, callback) {
   var fileReader = new FileReader();
   fileReader.addEventListener("load", function(e) {
     var buffer = e.target.result;
     var view = new Uint8Array(buffer);
+
+    // First extract the magic number - "P6"
     var magicNumber = toString([view[0], view[1]]);
     assert(magicNumber === "P6",
       "This program can only convert images from the P6 format (your format: " +
       magicNumber + ")");
+
+    // Then, extract whitespace.
     var i = 2;
     while (isWhitespace(view[i])) {
       ++i;
     }
+
+    // Then, extract the width.
     var widthChars = [];
     while (!isWhitespace(view[i])) {
       widthChars.push(view[i]);
@@ -80,10 +96,12 @@ var convertP6toP3 = function(p6File, callback) {
     assert(isFinite(width) && width > 0,
       "Invalid width specified in PPM file");
 
+    // Then, extract whitespace.
     while (isWhitespace(view[i])) {
       ++i;
     }
 
+    // Then, extract the height.
     var heightChars = [];
     while (!isWhitespace(view[i])) {
       heightChars.push(view[i]);
@@ -94,10 +112,12 @@ var convertP6toP3 = function(p6File, callback) {
     assert(isFinite(height) && height > 0,
       "Invalid height specified in PPM file");
 
+    // Then, extract whitespace.
     while (isWhitespace(view[i])) {
       ++i;
     }
 
+    // Then, extract the maximum color value.
     var maxValChars = [];
     while (!isWhitespace(view[i])) {
       maxValChars.push(view[i]);
@@ -110,16 +130,22 @@ var convertP6toP3 = function(p6File, callback) {
       "Maximum color value is not within the range (0, 65536)");
     var wideValues = maxVal >= 256;
 
+    // Create the new P3 file with the same width and height and a maximum color
+    //  value of 255.
     var newHeader = toBinary("P3\n" + width + " " + height + "\n" + newMaxVal +
       "\n");
     var newImage = new Uint8Array(
       newHeader.length + (width * 3 * 4 + 1) * height
     );
+
+    // Copy the header into the file.
     var j = 0;
     while (j < newHeader.length) {
       newImage[j] = newHeader[j];
       ++j;
     }
+
+    // Convert the binary P6 data into ASCII P3 data and append it to the file.
     var pixelNumber = 0;
     var value;
     var binary;
@@ -144,6 +170,8 @@ var convertP6toP3 = function(p6File, callback) {
     }
     assert(i == view.length && j == newImage.length,
       "Actual number of pixels did not match specified width and height");
+
+    // Call the callback with the new file.
     callback(new File([newImage.buffer], p6File.name, {
       type: "image/x-portable-pixmap"
     }));
@@ -152,6 +180,8 @@ var convertP6toP3 = function(p6File, callback) {
   fileReader.readAsArrayBuffer(p6File);
 };
 
+// download(file, name) - Automatically gives the user a prompt to download
+//  a file `file` with name `name`.
 var download = function(file, name) {
   if (name === undefined) {
     name = file.name;
@@ -165,10 +195,14 @@ var download = function(file, name) {
   document.body.removeChild(link);
 };
 
+// convertAndDownload(p6File) - Converts p6File to the P3 format and downloads
+//  it.
 var convertAndDownload = function(p6File) {
   convertP6toP3(p6File, download);
 };
 
+// handleFiles(fileList) - Converts and downloads each file in `fileList`.
+//  Produces an error if there are no files to handle.
 var handleFiles = function(fileList) {
   assert(fileList.length > 0, "Please select a file to convert");
   for (var i = 0; i < fileList.length; ++i) {
@@ -176,11 +210,15 @@ var handleFiles = function(fileList) {
   }
 };
 
+// When the "Convert" button is pressed, convert and download all selected
+//  files.
 var fileInputEl = document.getElementById("file-input");
 var convertButtonEl = document.getElementById("start-conversion");
 convertButtonEl.addEventListener("click", function() {
   handleFiles(fileInputEl.files);
 });
 
+// When the "X" button is pressed, hide the success/error overlay at the top of
+//  the page.
 var hideErrorButtonEl = document.getElementById("hide-error");
 hideErrorButtonEl.addEventListener("click", hideError);
